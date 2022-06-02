67520ffbafac3c416f12f47c3655af5ea3d9aa88
David J <d.jordan.a24@gmail.com>
2022-06-01 22:06:22 -0500

    algunos test con Junit y Mockito

--- src/test/java/com/jordy/javatest/util/StringUtilTest.java
+++ src/test/java/com/jordy/javatest/util/StringUtilTest.java
@@ -1,13 +1,27 @@
 package com.jordy.javatest.util;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StringUtilTest {
 
-    public static void main(String[] args) {
-        String result = StringUtil.repeat("hola",3);
+    @Test
+    public void repeat_string_once(){
+        Assert.assertEquals("hola",StringUtil.repeat("hola",1));
+    }
 
-        if (!result.equals("holaholahola")){
-            System.out.println("Error ");
-        }
+    @Test
+    public void repeat_string_multiple_times(){
+        Assert.assertEquals("holaholahola",StringUtil.repeat("hola",3));
+    }
+    @Test
+    public void repeat_string_zero_times(){
+        Assert.assertEquals("",StringUtil.repeat("hola",0));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void repeat_string_negative_times(){
+        StringUtil.repeat("hola",-2);
     }
 
 }
